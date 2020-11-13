1de8745543e2745a7da80bd8fde52b64bf42fe99
Larrypcdotcom <26318510+Larrypcdotcom@users.noreply.github.com>
2020-11-13 04:11:10 -0600

    Update JS implementation

--- js_implementation/runedata.js
+++ js_implementation/runedata.js
@@ -1,88 +1,131 @@
 const fs = require('fs');
 
-// INPUT: path to a CSV file with the following format:
-//
-// block_number,timestamp,pool_units,balance_rune,balance_asset
-// 157500,1598952349,20802124800,21682701709,19983007934
-// 158125,1598955799,20802124800,21684792109,19983007934
-// 158750,1598959250,20802124800,21686881587,19983007934
-// 159375,1598962701,20802124800,21688997816,19983007934
-// 160000,1598966150,20802124800,21691123830,19983007934
-//
-// Column headers must match exactly.
-//
-// OUTPUTS: object with the following format:
-//
-// {
-//     157500: {
-//         timestamp: 1598952349,
-//         poolUnits: 20802124800,
-//         balanceRune: 21682701709,
-//         balanceAsset: 19983007934,
-//     },
-//     158125: { ... },
-//     158750: { ... },
-//     ...
-// }
-//
-// The keys of this object are block numbers.
-//
+const _getKeysSorted = (obj) => {
+    var keys = Object.keys(obj);
+    keys = keys.map(x => +x);  // Cast strings to numbers
+    keys = keys.sort((a, b) => a - b);  // Sort ascendingly
+    return keys
+}
+
 const parseCSV = (path) => {
-    var lines = fs.readFileSync(path, { encoding: 'utf-8' }).split('\n');
-    lines = lines.slice(1, lines.length - 1);  // Remove header and the empty line at the very end
+    var rows = fs.readFileSync(path, { encoding: 'utf-8' }).split('\n');
+    rows = rows.slice(1, rows.length - 1);
 
     var data = {};
-    var line = undefined;
 
-    for ( i = 0; i < lines.length; i++ ) {
-        line = lines[i].split(',');
-        data[line[0]] = {
-            timestamp: line[1],
-            poolUnits: line[2],
-            balanceRune: line[3],
-            balanceAsset: line[4]
+    for ( i = 0; i < rows.length; i++ ) {
+        row = rows[i].split(',');
+        data[row[0]] = {
+            timestamp: parseInt(row[1]),
+            poolUnits: parseInt(row[2]),
+            balanceRune: parseInt(row[3]),
+            balanceAsset: parseInt(row[4])
         };
     }
 
     return data;
 };
 
-// INPUTS:
-//
-// amountInvestedUsd: float
-// timeInvested: int, timestamp in UTC
-// assetData: object, same format as output by parseCSV
-// busdData: object, same format as output by parseCSV
-//
-// The keys (block numbers) of assetData must be a subset of busdData.
-//
-// OUTPUTS: object with the following format:
-//
-// {
-//     157500: {
-//         timestamp: ... ,
-//         runePriceUsd: ... ,
-//         assetPriceUsd: ... ,
-//         runeBalance: ... ,
-//         assetBalance: ... ,
-//         runeValueUsd: ... ,
-//         assetValueUsd: ... ,
-//         totalValueUsd: ... ,
-//         ValueIfHoldRune: ... ,
-//         ValueIfHoldAsset: ... ,
-//         feeAccrual: ... ,
-//         impermLoss: ... ,
-//         totalGainsVsHold: ...
-//     },
-//     158125: { ... },
-//     158750: { ... },
-//     ...
-// }
-//
-const calculateUserData = (amountInvestedUsd, timeInvested, assetData, busdData) => {
-    //pass
+const calculateUsdPrices = (assetData, busdData) => {
+    var blockNumbers = _getKeysSorted(assetData);
+    var assetDataWithPrices = {};
+
+    for ( i = 0; i < blockNumbers.length; i++ ) {
+        blockNumber = blockNumbers[i];
+        blockAsset = assetData[blockNumber];
+        blockBusd = busdData[blockNumber];
+
+        runePriceUsd = blockBusd.balanceAsset / blockBusd.balanceRune;
+        assetPriceRune = blockAsset.balanceRune / blockAsset.balanceAsset;
+        assetPriceUsd = assetPriceRune * runePriceUsd;
+
+        totalPoolValue = blockAsset.balanceAsset * assetPriceUsd + blockAsset.balanceRune * runePriceUsd;
+        sharePriceUsd = totalPoolValue / blockAsset.poolUnits;
+
+        assetDataWithPrices[blockNumber] = {
+            ...blockAsset, runePriceUsd, assetPriceUsd, sharePriceUsd
+        };
+    }
+
+    return assetDataWithPrices;
 };
 
+const calculateUserData = (amountInvested, timeInvested, assetData) => {
+    // Find the first data point after timeInvested
+    var blockNumbers = _getKeysSorted(assetData);
+    for ( i = 0; i < blockNumbers.length; i++ ) {
+        if (assetData[blockNumbers[i]].timestamp >= timeInvested) {
+            break;
+        }
+    }
+
+    blockNumbers = blockNumbers.slice(i, blockNumbers.length);
+    var blockInvested = assetData[blockNumbers[0]];
+
+    var userShare = amountInvested / blockInvested.sharePriceUsd;
+    var userData = {};
+
+    for (i = 0; i < blockNumbers.length; i++) {
+        blockNumber = blockNumbers[i];
+        block = assetData[blockNumber];
+
+        // Balances
+        runeBalance = userShare * block.balanceRune / block.poolUnits;
+        assetBalance = userShare * block.balanceAsset / block.poolUnits;
+
+        // Values
+        runeValueUsd = runeBalance * block.runePriceUsd;
+        assetValueUsd = assetBalance * block.assetPriceUsd;
+        totalValueUsd = runeValueUsd + assetValueUsd;
+
+        if (i == 0) {
+            valueIfHoldRune = totalValueUsd;
+            valueIfHoldAsset = totalValueUsd;
+            valueIfHoldBothRuneAsset = totalValueUsd;
+
+            feeAccrual = 0.;
+            impermLoss = 0.;
+            totalGainsVsHold = 0.;
+        } else {
+            valueIfHoldRune = 2 * userData[blockNumbers[0]].runeBalance * block.runePriceUsd;
+            valueIfHoldAsset = 2 * userData[blockNumbers[0]].assetBalance * block.assetPriceUsd;
+            valueIfHoldBothRuneAsset = (valueIfHoldRune + valueIfHoldAsset) / 2;
+
+            kValue = runeBalance * assetBalance;
+            kValueInit = userData[blockNumbers[0]].runeBalance * userData[blockNumbers[0]].assetBalance;
+            feeAccrual = 0.5 * (kValue / kValueInit - 1);
+
+            relativePrice = block.assetPriceUsd / block.runePriceUsd;
+            relativePriceInit = blockInvested.assetPriceUsd / blockInvested.runePriceUsd;
+            priceSwing = relativePrice / relativePriceInit;
+            impermLoss = 2 * Math.sqrt(priceSwing) / (priceSwing + 1) - 1;
+
+            totalGainsVsHold = totalValueUsd / valueIfHoldBothRuneAsset - 1;
+        }
+
+        userData[blockNumber] = {
+            runeBalance, assetBalance, runeValueUsd, assetValueUsd, totalValueUsd,
+            valueIfHoldRune, valueIfHoldAsset, valueIfHoldBothRuneAsset,
+            feeAccrual, impermLoss, totalGainsVsHold
+        };
+        console.log(blockNumber, {
+            runeBalance, assetBalance, runeValueUsd, assetValueUsd, totalValueUsd,
+            valueIfHoldRune, valueIfHoldAsset, valueIfHoldBothRuneAsset,
+            feeAccrual, impermLoss, totalGainsVsHold
+        });
+    }
+
+    return userData;
+};
+
+module.exports = { parseCSV, calculateUsdPrices, calculateUserData }
+
 // Test
-busdData = parseCSV('../data/pool_BNB.BUSD-BD1.csv');
-btcbData = parseCSV('../data/pool_BNB.BTCB-1DE.csv');
+console.log(calculateUserData(
+    10000,  // amount of USD invested
+    1603324800,  // UNIX timestamp of the time invested
+    calculateUsdPrices(
+        parseCSV('../data/pool_BNB.BTCB-1DE.csv'),
+        parseCSV('../data/pool_BNB.BUSD-BD1.csv')
+    )  // data of the liquidity pool
+));
